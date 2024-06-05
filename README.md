81c3d3b19fbe04febeb097114f0703347e11a74a
laacrmxsa <ivanignaciopm@gmail.com>
2024-06-04 22:53:59 -0400

    final

---
+++ README.md
@@ -0,0 +1,19 @@
+# C Beginner Study: Arrays
+
+The main goal of this exercise is to visualize how C work with matrices and what is one of the common methods
+
+to get the size of a row, of a column and the matrix itself.
+
+By default C let you create a matrix with no number of rows defined:
+
+```
+int matrix[][4];
+```
+
+In this example the array could have any number of rows you want. **However the size of each row should be 4. This parameter can't be ommited.**
+
+You can try with this code adding a new row to the matrix, and see how the result changes. To try this you should:
+
+* [ ] Add a row to the matrix
+* [ ] Compile it with `gcc c_exercise.c -o c_exercise`
+* [ ] Run it with `./c_exercise`
---
+++ c_exercise.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+int main() {
+  // Define a 4x4 integer matrix
+  int matrix[][4] = {{14, 10, 6, 4}, {3, 7, 18, 11}, {13, 9, 5, 17}, {19, 12, 2, 1}}; 
+
+  // Get the total size of the matrix in bytes
+  int rowslength = sizeof(matrix);
+  // Get the size of one row of the matrix in bytes
+  int columnslength = sizeof(matrix[0]);
+  // Get the size of an integer in bytes
+  int integerSize = sizeof(int);
+
+  printf("Total size of the matrix = %d\n", rowslength); // 64
+  printf("Size of one row = %d\n", columnslength); // 16
+  printf("Size of an integer = %d\n", integerSize); // 4
+
+  // Calculate the number of rows in the matrix
+  // ~Divide the total size of the matrix by the size of one row
+  int rowDimension = sizeof(matrix) / sizeof(matrix[0]);
+
+  // Calculate the number of columns in the matrix
+  // ~Divide the size of one row by the size of an integer
+  int columnDimension = sizeof(matrix[0]) / sizeof(int);
+
+  printf("Number of rows = %d\n", rowDimension);  // 4
+  printf("Number of columns = %d\n", columnDimension);  // 4
+
+  return 0;
+}
+
+/*
+
+~Output for a (?)x4 matrix:
+
+    Total size of the matrix = 64
+    Size of one row = 16
+    Size of an integer = 4
+    Number of rows = 4
+    Number of columns = 4
+
+
+~Output for a (?)x5 matrix:
+
+    Total size of the matrix = 80
+    Size of one row = 20
+    Size of an integer = 4
+    Number of rows = 4
+    Number of columns = 5
+
+*/
